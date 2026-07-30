9231a10550622b0841dc1e83189b2c92499e9744
Mohammed Sufiyan U S <sufiyanus2000@gmail.com>
2026-07-30 18:49:57 +0530

    one more line

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,7 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
        System.out.println("Welcome to Java Track.");
+       System.out.println("I already how to know coding or builoding");
+       System.out.println("Now i m learning coding");
     }
 }
