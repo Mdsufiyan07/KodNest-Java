d97a076cd07345e630aa33329c52e626cd639b68
Mohammed Sufiyan U S <sufiyanus2000@gmail.com>
2026-08-06 15:47:47 +0530

    pgm0

---
+++ track/M02/T01/ST01/pgm2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class pgm2 {
+ public static void main(String[] args) {
+     Scanner scan = new Scanner (System.in);
+int n = scan.nextInt();
+switch (n){
+    case 1 : System.err.println("one");
+    break;
+    case 2 : System.err.println("two");
+    break;
+    case 3 : System.err.println("three");
+    break;
+    case 4 : System.err.println("four");
+    break;
+    case 5 : System.err.println("five");
+    break;
+    case 6 : System.err.println("6");
+    break;
+    case 7 : System.err.println("7");
+    break;
+    case 8 : System.err.println("8");
+    break;
+    case 9 : System.err.println("9");
+    break;
+    case 10 : System.err.println("10");
+    break;
+    default : System.err.println("Invalid");
+    break;
+}
+ }
+}
---
+++ track/M02/T01/ST01/pgm4.java
@@ -0,0 +1,8 @@
+public class pgm4 {
+ public static void main(String[] args) {
+     
+     for(int i=0;i<=5;i++){
+System.err.println(i);
+     }
+ }
+}
