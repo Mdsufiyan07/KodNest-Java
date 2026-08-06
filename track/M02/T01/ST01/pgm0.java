065cdf6e2fec694241a2fa0bc7bfe98ae69b434e
Mohammed Sufiyan U S <sufiyanus2000@gmail.com>
2026-08-06 16:29:56 +0530

    Add Day1 pgm files to M02 T01 ST01

---
+++ track/M02/T01/ST01/pgm0.java
@@ -0,0 +1,9 @@
+public class pgm0 {
+ public static void main(String[] args) {
+     int a = 10;
+     int res = a++ + a++ + ++a + a++ + ++a + a++ + a++;
+     System.out.println(a);
+     System.out.println(res);
+     
+ }
+}
