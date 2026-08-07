715a795eabc233c29557475e5b4d59a24e95cd8a
Mohammed Sufiyan U S <sufiyanus2000@gmail.com>
2026-08-07 14:39:20 +0530

    nested do while loop

---
+++ track/M02/T01/ST01/pgm9dowhile.java
@@ -0,0 +1,15 @@
+public class pgm9dowhile {
+public static void main(String[] args) {
+    int x =1;
+    do{
+        int y =1;
+        do{
+            System.out.println(y+" ");
+            y++;
+        }while(y<=5);
+        System.out.println();
+        x++;
+    }while(x<=5);
+    }
+    
+}
