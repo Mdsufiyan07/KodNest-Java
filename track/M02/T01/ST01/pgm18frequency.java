1f644b5a0e9d1139c56fb3ef753e83db6930b818
Mohammed Sufiyan U S <sufiyanus2000@gmail.com>
2026-08-18 16:18:45 +0530

    finding element count

---
+++ track/M02/T01/ST01/pgm18frequency.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class pgm18frequency {
+public static void main(String[] args) {
+     Scanner s = new Scanner(System.in);
+    System.out.println("Enter size: ");
+    int size = s.nextInt();//size
+    int a[] = new int[size];//array creation
+    System.out.println("Enter Key to find: ");//key input
+    int key = s.nextInt();
+    System.out.println("Enter elements: ");
+    for(int i =0; i<size;i++){
+        a[i] = s.nextInt();//input elements
+    }
+frequency s1= new frequency();
+s1.freq(a, key);
+
+}
+}
+
+class frequency{
+    int count = 0;
+    void freq(int a[],int k){
+        
+        for(int i=0;i<a.length;i++){
+            if(a[i]==k){
+                count++;
+            }
+        }
+        System.out.println("The frequency of "+k+" is "+count);
+    }
+}
