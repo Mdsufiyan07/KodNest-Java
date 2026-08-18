cae40adc465e0e84cbe75d173504d5f0e4318706
Mohammed Sufiyan U S <sufiyanus2000@gmail.com>
2026-08-18 14:24:38 +0530

    array

---
+++ track/M02/T01/ST01/pgm15summofarray.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class pgm15summofarray {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a[]=new int[5];
+    System.out.println("Enter array elements : ");
+    for(int i =0 ;i<a.length;i++){
+        a[i]= sc.nextInt();
+    }
+int sum =0;
+for(int i =0 ;i<a.length;i++){
+        sum+=a[i];
+    }
+    System.out.println("sum of array elements is : "+sum);
+}
+}
