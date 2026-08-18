21579682b5a5b6f3d8aaf69d2c0a940d0fdfdc5e
Mohammed Sufiyan U S <sufiyanus2000@gmail.com>
2026-08-18 16:30:44 +0530

    finding element count

--- track/M02/T01/ST01/pgm17findkey.java
+++ track/M02/T01/ST01/pgm17findkey.java
@@ -3,33 +3,43 @@ public class pgm17findkey {
 public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
     System.out.println("Enter size: ");
+    
+    //taking size input
     int size = s.nextInt();
     int a[] = new int[size];
+
+    //target entry for finding in array
     System.out.println("Enter Key to find: ");
     int key = s.nextInt();
+
+    //entering elements in array
     System.out.println("Enter elements: ");
     for(int i =0; i<size;i++){
         a[i] = s.nextInt();
     }
+    //making object
 search s1= new search();
+
+//function call
 s1.searchKey(a, key);
-
 }
 }
 
+//search class  
 class search{
+    boolean f = false;
     void searchKey(int a[],int k){
-        boolean f = false;
+        
         for(int i=0;i<a.length;i++){
 if(a[i]==k){
-    System.err.println("The element "+k+" found in index: "+i);
+    System.out.println("The element "+k+" found in index: "+i);
     f=true;
     break;
 }
 }
 
 if(!f){
-    System.out.println("element is not found");
+    System.out.println("The element"+k+"is not found");
 }
 
     }
--- track/M02/T01/ST01/pgm18frequency.java
+++ track/M02/T01/ST01/pgm18frequency.java
@@ -4,15 +4,24 @@ public class pgm18frequency {
 public static void main(String[] args) {
      Scanner s = new Scanner(System.in);
     System.out.println("Enter size: ");
+
     int size = s.nextInt();//size
+
     int a[] = new int[size];//array creation
+
     System.out.println("Enter Key to find: ");//key input
     int key = s.nextInt();
+
+    //input elements
     System.out.println("Enter elements: ");
     for(int i =0; i<size;i++){
-        a[i] = s.nextInt();//input elements
+        a[i] = s.nextInt();
     }
+
+    //making object
 frequency s1= new frequency();
+
+//function call
 s1.freq(a, key);
 
 }
