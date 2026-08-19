847d28e6a6154f9f52a29eec4444c8dbe2c8fa1d
Mohammed Sufiyan U S <sufiyanus2000@gmail.com>
2026-08-19 15:05:32 +0530

    2D array

--- track/M02/T01/ST01/pgm19TwoDimensionalArray.java
+++ track/M02/T01/ST01/pgm19TwoDimensionalArray.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class pgm19TwoDimensionalArray {
 public static void main(String[] args) {
    Scanner sc = new Scanner(System.in);
-   int i,j,x,y;
+   int x,y;
    System.out.println("Enter number of rows: ");
    x=sc.nextInt();
    System.out.println("Enter number of colmuns: ");
@@ -11,17 +11,16 @@ public static void main(String[] args) {
    System.out.println("Enter elements: ");
    int a[][]=new int[x][y]; 
 
-   for(i=0;i<x;i++){
-    for(j=0;j<y;j++){
-       a[i][j]=sc.nextInt();
+   for(x=0;x<a.length;x++){
+    for(y=0;y<a[x].length;y++){
+       a[x][y]=sc.nextInt();
     }
-    System.out.println(); 
    }
 System.out.println("Array elements are: ");
    
-   for(i=0;i<x;i++){
-    for(j=0;j<y;j++){
-       System.out.print(a[i][j]+" ");
+   for(x=0;x<a.length;x++){
+    for(y=0;y<a[x].length;y++){
+       System.out.print(a[x][y]+" ");
     }
     System.out.println();
    }
